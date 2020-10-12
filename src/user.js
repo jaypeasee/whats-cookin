f3010c83240a229070df0b295514aba67c591230
JP Carey <jpcarey4@gmail.com>
2020-10-12 16:50:32 -0600

    Add search, add methods to User class

--- src/user.js
+++ src/user.js
@@ -1,15 +1,61 @@
 class User {
-  constructor(name, id, pantry) {
+  constructor(name, id, pantry, recipes) {
     this.name = name;
     this.id = id;
     this.pantry = pantry;
-    this.favoritesRecipes = [];
+    this.recipes = recipes;
+    this.favoriteRecipes = [];
     this.shoppingList = [];
     this.recipesToCook = [];
   }
 
   addFavorite(recipe) {
-    this.favoritesRecipes.push(recipe);
+    this.favoriteRecipes.push(recipe);
+  }
+
+  removeFavorite(id) {
+    this.favoriteRecipes.forEach((recipe, index) => {
+      if (id === recipe.id) {
+        this.favoriteRecipes.splice(index, 1);
+      }
+    });
+  }
+
+  searchFavorites(nameFragment) {
+    const filteredFavorites = this.favoriteRecipes.filter(recipe => {
+      return recipe.name.includes(nameFragment);
+    });
+    return filteredFavorites;
+  }
+
+  searchAllRecipes(nameFragment) {
+    const filteredRecipes = this.recipes.filter(recipe => {
+      return recipe.name.includes(nameFragment);
+    });
+    return filteredRecipes;
+  }
+
+  searchByTag(tagName) {
+    const filteredTags = this.recipes.filter(recipe => {
+      return recipe.tags.includes(tagName);
+    });
+    return filteredTags;
+  }
+
+  chooseRecipe(recipe) {
+    //loop through pantry.
+    //loop through recipe ingredients
+    //if item.id === recipe.ingredients.id
+    //and if item.quantity >= recipe.ingredients.quantity
+    //push into this.recipesToCook
+
+    //else
+    //create variable of amount
+    //recipe.ingredients.quantiy - item.quantity
+    //new object assigned to
+    // name: recipe.ingredients.name
+    //amount: amount
+    //cost: amount * recipe.ingredients.estimatedCostInCents
   }
 }
 
--- test/user-test.js
+++ test/user-test.js
@@ -11,11 +11,11 @@ describe('User', () => {
   let pantry1;
   let pantry2;
   let recipe1;
+  let recipe2;
+  let recipe3;
   let ingredient1;
 
   beforeEach( () => {
-    user1 = new User('Coop', 1, pantry1);
-    user2 = new User('JP', 2, pantry2);
 
     pantry1 = [{ ingredient: 55, amount: 4 }, { ingredient: 1234, amount: 0 }, { ingredient: 4321, amount: 10 }];
 
@@ -24,9 +24,14 @@ describe('User', () => {
     ingredient1 = new Recipe(1, "rosemary", 300);
     ingredient2 = new Recipe(2, "thai mayo", 666);
 
-    recipe1 = new Recipe(1, "Big image thing http", [ingredient1, ingredient2], [{ instruction: "boil em", number: 1 }, { instruction: "mash em", number: 2 }, { instruction: "stick em in a stew", number: 3 }], "Gollems favorite", ["rabbit", "banana", "yup"]);
+    recipe1 = new Recipe(1, "Big image thing http", [ingredient1, ingredient2], [{ instruction: "boil em", number: 1 }, { instruction: "mash em", number: 2 }, { instruction: "stick em in a stew", number: 3 }], "pasta sauce", ["rabbit", "banana", "yup", "garlic"]);
 
-    recipe2 = new Recipe()
+    recipe2 = new Recipe(2, "another image", [ingredient1, ingredient2], [{ instruction: "chop it up", number: 2 }, { instruction: "put some butter on it", number: 2 }], "Garlic bread", ["bread", "garlic", "Italian"])
+
+    recipe3 = new Recipe(3, "jpg", [ingredient2, ingredient1], [{ instruction: "boil water", number: 1 }, { instruction: "add salt", number: 2 }, { instruction: "soften noodles", number: 3 }], "pasta", ["pasta", "carbs", "Italian"])
+
+    user1 = new User('Coop', 1, pantry1, [recipe1, recipe2, recipe3]);
+    user2 = new User('JP', 2, pantry2, [recipe2, recipe3, recipe1]);
   });
 
   it('should be a function', () => {
@@ -61,8 +66,16 @@ describe('User', () => {
     expect(user2.pantry).to.deep.equal(pantry2);
   });
 
+  it('should be able to have a list of recipes', () => {
+    expect(user1.recipes).to.deep.equal([recipe1, recipe2, recipe3]);
+  });
+
+  it('should be able to have a different list of recipes', () => {
+    expect(user2.recipes).to.deep.equal([recipe2, recipe3, recipe1])
+  })
+
   it('should have no favorite recipes to start', () => {
-    expect(user1.favoritesRecipes).to.deep.equal([]);
+    expect(user1.favoriteRecipes).to.deep.equal([]);
   });
 
   it('should have an empty shopping list to start', () => {
@@ -76,16 +89,50 @@ describe('User', () => {
   it('should be able to add to their favorite recipes', () => {
     user1.addFavorite(recipe1);
 
-    expect(user1.favoritesRecipes[0]).to.equal(recipe1);
+    expect(user1.favoriteRecipes[0]).to.equal(recipe1);
   });
 
   it('should be able to remove from their favorite recipes', () => {
     user1.addFavorite(recipe1);
+    user1.addFavorite(recipe2);
+    user1.addFavorite(recipe3);
 
-    user1.removeFavorite(recipe1.id);
+    user1.removeFavorite(recipe2.id);
 
+    expect(user1.favoriteRecipes[1]).to.equal(recipe3);
+  });
 
-  })
+  it('should be able to search favorite recipes by name', () => {
+    user1.addFavorite(recipe1);
+    user1.addFavorite(recipe2);
+    user1.addFavorite(recipe3);
 
+    expect(user1.searchFavorites("Garlic bread")).to.deep.equal([recipe2]);
+  });
 
+  it('should be able to search for multiple favorites by name fragment', () => {
+    user1.addFavorite(recipe1);
+    user1.addFavorite(recipe2);
+    user1.addFavorite(recipe3);
+
+    expect(user1.searchFavorites("past")).to.deep.equal([recipe1, recipe3]);
+  });
+
+  it('should be able to search through all recipes by name', () => {
+    expect(user1.searchAllRecipes("past")).to.deep.equal([recipe1, recipe3]);
+  });
+
+  it('should be able to search recipes by tag', () => {
+    expect(user1.searchByTag("Italian")).to.deep.equal([recipe2, recipe3]);
+  });
+
+  it.skip('should be able to search by multiple tags', () => {
+    user1.searchByTag("Italian");
+
+    expect(user1.searchByTag("garlic")).to.deep.equal([recipe2]);
+  });
+
+  it.skip('should be able to cook a recipe if ingredients are available', () => {
+    user1.chooseRecipe(recipe1);
+  });
 });
