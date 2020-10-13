69d7193d742071bceac53c563a45d960b746fa5e
Cooper Terrones <cooper.terrones@outlook.com>
2020-10-13 15:36:16 -0600

    Load recipe cards on page load

--- src/scripts.js
+++ src/scripts.js
@@ -1,2 +1,27 @@
-const chai = require('chai');
-const expect = chai.expect
+const pageWrap = document.querySelector('.recipes-wrapper');
+
+console.log(recipeData);
+
+window.addEventListener('load', loadRecipes);
+
+function loadRecipes() {
+  recipeData.forEach((recipe) => {
+    let newRecipe = new Recipe(recipe.id, recipe.image, recipe.ingredients, recipe.instructions, recipe.name, recipe.tags)
+    let recipeCardInfo = `<div class="recipe-card">
+      <img src=${newRecipe.image}>
+      <p>${newRecipe.name}</p>
+      <div class="tag-buttons">
+        <button type="button" name="button">Tag</button>
+        <button type="button" name="button">Tag</button>
+        <button type="button" name="button">Tag</button>
+      </div>
+    </div>`
+    pageWrap.insertAdjacentHTML('afterbegin', recipeCardInfo);
+  });
+}
+
+//instantiate the recipe class
+
+//insert adjacent html on the recipes-wrapper
+
+//insert a specific card
